7e86d485bb75508fbc165bf7109e597fd0cd88e3
grosmar <zubak.balazs@gmail.com>
2016-09-22 09:42:53 +0200

    Refactored HaxeDevelop project and filtered to js

--- src/tictactoe/TicTacToe.hx
+++ src/tictactoe/TicTacToe.hx
@@ -3,10 +3,12 @@ package tictactoe;
 import hex.compiler.parser.xml.XmlCompiler;
 import hex.log.layout.JavaScriptConsoleLayout;
 import hex.log.layout.LogProxyLayout;
+import tictactoe.module.game.GameModule;
+#if js
 import js.Browser;
 import js.tictactoe.view.game.ActivePlayerIndicatorView;
 import js.tictactoe.view.game.BoardView;
-import tictactoe.module.game.GameModule;
+#end
 
 /**
  * ...
@@ -19,7 +21,9 @@ class TicTacToe
 	{
 		#if debug
 			var proxy : hex.log.layout.LogProxyLayout = new hex.log.layout.LogProxyLayout();
-			proxy.addListener( new hex.log.layout.JavaScriptConsoleLayout() );
+			#if js
+				proxy.addListener( new hex.log.layout.JavaScriptConsoleLayout() );
+			#end
 		#end
 		
 		new TicTacToe();
@@ -27,11 +31,12 @@ class TicTacToe
 	
 	public function new()
 	{
-		var game = new GameModule( 
-			new ActivePlayerIndicatorView( Browser.document.querySelector(".player-list") ), 
-			new BoardView( Browser.document.querySelector(".tic-tac-toe-table") ) );
-		game.initialize();
-		//XmlCompiler.readXmlFile( "tictactoe/config/context.xml" );
+		#if js 
+			var game = new GameModule( 
+				new ActivePlayerIndicatorView( Browser.document.querySelector(".player-list") ), 
+				new BoardView( Browser.document.querySelector(".tic-tac-toe-table") ) );
+			game.initialize();
+		#end
 	}
 	
 }
